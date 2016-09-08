ac3eaa5daa50f60eafd7a50d60ef639b98e0f84d
AIMX <hectoraimx@gmail.com>
2016-09-08 16:13:38 +0200

    Finished!

---
+++ app.js
@@ -0,0 +1,52 @@
+/*(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.dishes = "Yaakov";
+  $scope.output = "hungry";
+
+  $scope.calculateTooMuch = function () {
+    $scope.output = "Works!";
+  };
+}
+
+})();*/
+
+(function (){
+'use strict';
+
+angular.module ("LunchCheck", [])
+
+.controller("LunchCheckController", LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController ($scope) {
+	$scope.dishes = "";
+	$scope.output = "";
+	$scope.dishNumber=0;
+
+	$scope.calculateTooMuch = function () {
+
+		var out = calculateDishNumber($scope.dishes, $scope);
+
+		$scope.output = out;
+	};
+}
+
+function calculateDishNumber(string, $scope) {
+    var numberOfDishes = 0;
+    numberOfDishes = string.split(",").length;
+    $scope.dishNumber = numberOfDishes;
+
+    if(numberOfDishes>3){
+			return "Too Much!";
+	}else{
+			return "Enjoy!";
+	}
+  }
+
+})();
